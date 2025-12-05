08c0beff67f9595eda559905e58f86a87183a304
Sashank Bhamidi <hello@sashank.wiki>
2025-12-05 17:56:25 +0530

    docs: configure Sphinx with PyData theme

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,57 @@ project = "icalendar-anonymizer"
 copyright = "2025, icalendar-anonymizer contributors"  # noqa: A001
 author = "icalendar-anonymizer contributors"
 
-extensions = ["sphinx.ext.autodoc"]
+# Extensions
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "myst_parser",
+]
 
-html_theme = "alabaster"
+# Theme configuration
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/mergecal/icalendar-anonymizer",
+    "use_edit_page_button": True,
+    "show_toc_level": 2,
+    "navbar_align": "left",
+}
+
+html_context = {
+    "github_user": "mergecal",
+    "github_repo": "icalendar-anonymizer",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "icalendar": ("https://icalendar.readthedocs.io/en/latest/", None),
+}
+
+# MyST parser configuration
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+
+# Napoleon settings (Google-style docstrings)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+
+# Autodoc settings
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+
+# Autosectionlabel settings
+autosectionlabel_prefix_document = True
